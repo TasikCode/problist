6b500ce83ed94ee6c73355751425979596b2763a
Dagus Wijaya <dagus.wijaya@gmail.com>
2019-10-08 21:01:33 +0700

    Create home
    temporary home controller and view

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->load->view('template/head');
+		$this->load->view('home');
+		$this->load->view('template/foot');
+	}
+
+}
+
+/* End of file Home.php */
+/* Location: ./application/controllers/Home.php */
