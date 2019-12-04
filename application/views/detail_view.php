5549a1bc222d3413e96e343f47cb09013ac95a70
Dagus Wijaya <dagus.wijaya@gmail.com>
2019-12-04 11:14:22 +0700

    Add disqus

--- application/views/detail_view.php
+++ application/views/detail_view.php
@@ -45,6 +45,33 @@
 		      		</div>
 		      		<div class="col-md-4"></div>
 		  		</div>
+			<div class="row" style="margin-top:40px; margin-bottom:40px">
+		      		<div class="col-md-2"></div>
+		      		<div class="col-md-8 text-center">
+		      			<h4 style="color:#2c3e50">Komentar</h4><br><br>
+		      			<div id="disqus_thread"></div>
+							<script>
+
+							/**
+							*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+							*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+							var disqus_config = function () {
+							this.page.url = '<?php echo base_url(uri_string());?>';  // Replace PAGE_URL with your page's canonical URL variable
+							this.page.identifier = '<?php echo base_url(uri_string());?>'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+							};
+
+							(function() { // DON'T EDIT BELOW THIS LINE
+							var d = document, s = d.createElement('script');
+							s.src = 'https://lapor-kabayan.disqus.com/embed.js';
+							s.setAttribute('data-timestamp', +new Date());
+							(d.head || d.body).appendChild(s);
+							})();
+							</script>
+							<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+		      		</div>
+		      		<div class="col-md-2"></div>
+		  		</div>
         </div>
     </section>
     <div aria-live="polite" aria-atomic="true" style="position: relative;">
